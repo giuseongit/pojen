dba9b5831266d92b0b0c89c4c8b8ff7283a220d0
Giuseppe Pagano <giuseppe.pagano.p@gmail.com>
2016-08-27 20:23:43 +0200

    added file handler

--- pojen/file_handler.py
+++ pojen/file_handler.py
@@ -1,16 +1,21 @@
 import os
 
+sep = os.path.sep
+
 def read(path):
     to_read = open(path, "r")
     content = to_read.read()
     to_read.close()
     return content
 
-def save(path, content)
-    to_write = open(path, "w")
+def save(path, content):
+    to_write = open(path, "w+")
     to_write.write(content)
     to_write.close()
 
 def create_folder_if_not_exist(path):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def file_exsits(path):
+    return os.path.exists(path)
