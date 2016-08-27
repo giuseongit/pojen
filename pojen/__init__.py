17e29e0218232115945254114d40b0866c960474
Giuseppe Pagano <giuseppe.pagano.p@gmail.com>
2016-08-27 20:27:08 +0200

    added main function

--- pojen/__init__.py
+++ pojen/__init__.py
@@ -1,6 +1,9 @@
 import json_parser
 import file_handler
 import engine
+from optparse import OptionParser
+import sys
+
 def process_json(jsonpath, outputpath, topclassname, public_fields=False, getset=True):
     to_load = file_handler.read(jsonpath)
     file_handler.create_folder_if_not_exist(outputpath)
@@ -22,3 +25,38 @@ def process_json(jsonpath, outputpath, topclassname, public_fields=False, getset
         path = "{}{}{}.java".format(outputpath, file_handler.sep, classname)
         file_handler.save(path, text)
 
+def main():
+    parser = OptionParser()
+    parser.usage =  "pojen FILENAME [TOPCLASSNAME] [options]\nFILENAME is the path of the input json\n"
+    parser.usage += "TOPCLASSNAME is the name of the top class (if any)"
+    parser.add_option("-o", "--output-folder", dest="output",
+                            help="specify output folder")
+    parser.add_option("-p", "--public-fields", dest="public_fields", action="store_true",
+                            help="specify if the fileds in the classes must be public")
+    parser.add_option("-g", "--get-set", dest="getset", action="store_false",
+                            help="specify if the classes must have getters and setters")
+    (options, args) = parser.parse_args()
+
+    if len(args) > 2 or len(args) < 1:
+        print >>sys.stderr, 'Wrong number of parameters!'
+        parser.print_help()
+        exit(-1)
+
+    output = options.output if options.output else "output"
+
+    if not file_handler.file_exsits(args[0]):
+        print >>sys.stderr, 'File {} does not exists!'.format(args[0])
+        exit(-1)
+
+    if len(args) == 2:
+        topclassname = args[1]
+    else:
+        topclassname = ""
+
+    process_json(
+        args[0], 
+        output,
+        topclassname,
+        options.public_fields if options.public_fields else False,
+        options.getset if options.getset else True
+    )
